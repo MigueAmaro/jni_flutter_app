bec9cdd047704b84d06350d87e74f50a608e72a1
Miguel Amaro <55157012+MigueAmaro@users.noreply.github.com>
2025-01-28 12:50:56 +0100

    adding macOS support and xcode lib of the .dylib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:ffi' as ffi;          // Core Dart FFI
-import 'dart:io' show Platform;    // Detect if running on Android
-import 'package:ffi/ffi.dart' as ffi_utc; // For easy Utf8 conversion
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform;
+import 'package:ffi/ffi.dart' as ffi_utc;
 import 'package:flutter/material.dart';
 
 // 1. Define the C function signatures
@@ -34,32 +34,31 @@ class _MyAppState extends State<MyApp> {
 
   void initFFI() {
     if (Platform.isAndroid) {
+      // Load the Android library
       _helloLib = ffi.DynamicLibrary.open('libhello.so');
-
-      // Lookup the 'helloFromC' symbol
-      _helloFromC = _helloLib
-          .lookup<ffi.NativeFunction<HelloFromCFunc>>('helloFromC')
-          .asFunction<HelloFromCDart>();
-
-      // Call 'helloFromC'
-      final ptr = _helloFromC(); // Returns a pointer to a C string
-
-      // Cast to Pointer<Int8> so we can interpret it as UTF8 text
-      // Cast to Pointer<Utf8>, not Pointer<Int8> after trying the above and doesnt work
-      final messagePtr = ptr.cast<ffi_utc.Utf8>();
-
-      // Use the 'toDartString()' extension method
-      final dartString = messagePtr.toDartString();
-
-
-      setState(() {
-        _message = dartString;
-      });
+    } else if (Platform.isMacOS) {
+      // Load the macOS library
+      _helloLib = ffi.DynamicLibrary.open('libhello.dylib');
     } else {
       setState(() {
-        _message = 'Not running on Android device/emulator :(.';
+        _message = 'Not running on Android or macOS.';
       });
+      return;
     }
+
+    // Lookup the 'helloFromC' symbol
+    _helloFromC = _helloLib
+        .lookup<ffi.NativeFunction<HelloFromCFunc>>('helloFromC')
+        .asFunction<HelloFromCDart>();
+
+    // Call 'helloFromC'
+    final ptr = _helloFromC();
+    final messagePtr = ptr.cast<ffi_utc.Utf8>();
+    final dartString = messagePtr.toDartString();
+
+    setState(() {
+      _message = dartString;
+    });
   }
 
   @override
